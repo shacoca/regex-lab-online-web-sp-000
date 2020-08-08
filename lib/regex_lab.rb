c53d4b6e795fe9228a0b76350219325a1147f6ea
shawn campbell <shacoca@yahoo.com>
2020-08-08 01:32:49 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
 #  word.match(/^[aeiou]/) ? true : false
-  if word.scan(/^[aeiou]/)
+  if word.scan(/^[aeiou]+/)
     true
-  elsif word.scan(/^[^aeiou]/)
+  elsif word.scan(/^[^aeiou]+/)
 #  else
     false
   end
