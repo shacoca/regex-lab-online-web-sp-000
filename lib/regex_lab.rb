1509512528702cee4a6b6628dc49f984633a10ca
shawn campbell <shacoca@yahoo.com>
2020-08-10 07:28:53 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z][\w\s]+/) != []
+  if text.scan(/^[A-Z].*[\.!?]$/) != []
 #  if (text[0].scan(/[A-Z]/)) != [] && (text[-1].scan(/\w/)) != []
     true
   else
@@ -25,9 +25,18 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  number_array = phone.scan(/(\d+)-(\d+)-(\d+)/)
-  if (number_array[0][0].length == 3) && (number_array[0][1].length == 3) && (number_array[0][2].length == 4)
-    true
+  # valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
+  # 2438894546 (718)891-1313 234 435 9978 (800)4261134
+  number_array = phone.scan(/.*(\d{3}).*(\d{3}).*(\d{4})/)
+#  number_array = 2438894546.scan(/.*(\d{3}).*(\d{3}).*(\d{4})/)
+#  number_array = (718)891-1313.scan(/.*(\d{3}).*(\d{3}).*(\d{4})/)
+#  number_array = 234 435 9978.scan(/.*(\d{3}).*(\d{3}).*(\d{4})/)
+  if number_array[0]
+    if (number_array[0][0].length == 3) && (number_array[0][1].length == 3) && (number_array[0][2].length == 4)
+      true
+    else
+      false
+    end
   else
     false
   end
